d001c9fa477262528f0913bbf920388dac28d27f
Kai Chen <22359229+kx-chen@users.noreply.github.com>
2022-03-16 15:00:01 -0700

    handler.js: Add basic proof of concept

--- src/ivr/handler.js
+++ src/ivr/handler.js
@@ -7,12 +7,12 @@ exports.welcome = function welcome() {
     action: '/ivr/menu',
     numDigits: '1',
     method: 'POST',
+    timeout: 30,
   });
 
   gather.say(
-    'Thanks for calling the E T Phone Home Service. ' +
-    'Please press 1 for directions. ' +
-    'Press 2 for a list of planets to call.',
+    'i am a robot and i can say words such as spagetti and spinach and continue um' + 
+    'press a key and i will try my best to repeat it back to you',
     {loop: 3}
   );
 
@@ -20,93 +20,15 @@ exports.welcome = function welcome() {
 };
 
 exports.menu = function menu(digit) {
-  const optionActions = {
-    '1': giveExtractionPointInstructions,
-    '2': listPlanets,
-  };
+  console.log("Got some digits!");
+  console.log(digit);
 
-  return (optionActions[digit])
-    ? optionActions[digit]()
-    : redirectWelcome();
-};
-
-exports.planets = function planets(digit) {
-  const optionActions = {
-    '2': '+12024173378',
-    '3': '+12027336386',
-    '4': '+12027336637',
-  };
-
-  if (optionActions[digit]) {
-    const twiml = new VoiceResponse();
-    twiml.dial(optionActions[digit]);
-    return twiml.toString();
-  }
-
-  return redirectWelcome();
-};
-
-/**
- * Returns Twiml
- * @return {String}
- */
-function giveExtractionPointInstructions() {
   const twiml = new VoiceResponse();
-
   twiml.say(
-    'To get to your extraction point, get on your bike and go down ' +
-    'the street. Then Left down an alley. Avoid the police cars. Turn left ' +
-    'into an unfinished housing development. Fly over the roadblock. Go ' +
-    'passed the moon. Soon after you will see your mother ship.',
-    {voice: 'alice', language: 'en-GB'}
+    'you just pressed ' + digit
   );
-
-  twiml.say(
-    'Thank you for calling the ET Phone Home Service - the ' +
-    'adventurous alien\'s first choice in intergalactic travel'
-  );
-
-  twiml.hangup();
-
-  return twiml.toString();
-}
-
-/**
- * Returns a TwiML to interact with the client
- * @return {String}
- */
-function listPlanets() {
-  const twiml = new VoiceResponse();
-
-  const gather = twiml.gather({
-    action: '/ivr/planets',
-    numDigits: '1',
-    method: 'POST',
-  });
-
-  gather.say(
-    'To call the planet Broh doe As O G, press 2. To call the planet DuhGo ' +
-    'bah, press 3. To call an oober asteroid to your location, press 4. To ' +
-    'go back to the main menu, press the star key ',
-    {voice: 'alice', language: 'en-GB', loop: 3}
-  );
-
-  return twiml.toString();
-}
-
-/**
- * Returns an xml with the redirect
- * @return {String}
- */
-function redirectWelcome() {
-  const twiml = new VoiceResponse();
-
-  twiml.say('Returning to the main menu', {
-    voice: 'alice',
-    language: 'en-GB',
-  });
 
   twiml.redirect('/ivr/welcome');
 
   return twiml.toString();
-}
+};
